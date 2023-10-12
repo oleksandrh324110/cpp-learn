ef3111738e54528009b752ef085adbbb5f0b1166
Oleksandr Havrylutsa <alex.mercer.324110@gmail.com>
2023-10-12 11:24:02 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,19 @@
-#include <array>
 #include <iostream>
-#include <vector>
 
-void print_tower(const std::vector<char> &tower) {
-  for (auto i : tower) {
-    std::cout << (int)i << " ";
+char *strcpy(char *str1, const char *str2) {
+  char *saved = str1;
+  while (*str2) {
+    *str1++ = *str2++;
   }
-}
-
-void swap_disks(std::vector<char> &from, std::vector<char> &to) {
-  to.push_back(from.back());
-  from.pop_back();
-}
-
-void hanoi_tower(unsigned short n) {
-  std::array<std::vector<char>, 3> towers{};
-
-  for (int i = 0; i < n; i++) {
-    towers[0].push_back(n - i);
-  }
-
-  char from, to;
-  while (towers[2].size() < n) {
-    std::cout << "tower a: ";
-    print_tower(towers[0]);
-    std::cout << "\ntower b: ";
-    print_tower(towers[1]);
-    std::cout << "\ntower c: ";
-    print_tower(towers[2]);
-
-    std::cout << "\n\n";
-
-    std::cout << "Move from: ";
-    std::cin >> from;
-    std::cout << "Move to: ";
-    std::cin >> to;
-
-    std::cout << "\n";
-
-    char diskFrom = std::tolower(from) - 'a';
-    char diskTo = std::tolower(to) - 'a';
-
-    if (diskFrom >= 0 && diskFrom <= 2 && diskTo >= 0 && diskTo <= 2) {
-      if (!towers[diskFrom].empty()) {
-        if (towers[diskTo].empty() ||
-            towers[diskFrom].back() < towers[diskTo].back()) {
-          swap_disks(towers[diskFrom], towers[diskTo]);
-        }
-      }
-    }
-  }
-
-  std::cout << "You solved it!\n";
+  *str1 = '\0';
+  return saved;
 }
 
 int main() {
-  std::cout << "Enter the number of disks: ";
-  unsigned short n;
-  std::cin >> n;
-  std::cout << "\n";
+  char str1[] = "";
+  char str2[] = "Hello World!";
 
-  hanoi_tower(n);
+  strcpy(str1, str2);
+
+  std::cout << "str1: " << str1 << '\n' << "str2: " << str2 << '\n';
 }
