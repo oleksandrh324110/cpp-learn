287c9d530d2e8988274544f961f296939ca874c4
Oleksandr Havrylutsa <alex.mercer.324110@gmail.com>
2023-10-19 08:19:17 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,28 @@
+#include <ctime>
 #include <iostream>
+#include <random>
 
-int main() {}
+int main() {
+  srand(time(nullptr));
+
+  int arr[3][4] = {};
+
+  int zeroCount = 0;
+
+  for (auto &i : arr)
+    for (auto &j : i)
+      j = rand() % 10;
+
+  for (auto &i : arr) {
+    for (auto &j : i)
+      std::cout << j << ' ';
+    std::cout << '\n';
+  }
+
+  for (auto &i : arr)
+    for (auto &j : i)
+      if (j == 0)
+        zeroCount++;
+
+  std::cout << "Zero count: " << zeroCount << '\n';
+}
