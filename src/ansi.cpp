5b3cdb10700b625abd8b2a6e064b47b2e1301dc8
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2023-10-24 12:34:43 +0300

    .

--- src/ansi.cpp
+++ src/ansi.cpp
@@ -1,23 +1,23 @@
 struct ANSI {
-  static constexpr char reset[] = "\033[0m";
+  static const constexpr char *reset = "\033[0m";
 
-  static constexpr char red[] = "\033[31m";
-  static constexpr char green[] = "\033[32m";
-  static constexpr char yellow[] = "\033[33m";
-  static constexpr char blue[] = "\033[34m";
-  static constexpr char magenta[] = "\033[35m";
-  static constexpr char cyan[] = "\033[36m";
-  static constexpr char white[] = "\033[37m";
+  static const constexpr char *red = "\033[31m";
+  static const constexpr char *green = "\033[32m";
+  static const constexpr char *yellow = "\033[33m";
+  static const constexpr char *blue = "\033[34m";
+  static const constexpr char *magenta = "\033[35m";
+  static const constexpr char *cyan = "\033[36m";
+  static const constexpr char *white = "\033[37m";
 
-  static constexpr char bgRed[] = "\033[41m";
-  static constexpr char bgGreen[] = "\033[42m";
-  static constexpr char bgYellow[] = "\033[43m";
-  static constexpr char bgBlue[] = "\033[44m";
-  static constexpr char bgMagenta[] = "\033[45m";
-  static constexpr char bgCyan[] = "\033[46m";
-  static constexpr char bgWhite[] = "\033[47m";
+  static const constexpr char *bgRed = "\033[41m";
+  static const constexpr char *bgGreen = "\033[42m";
+  static const constexpr char *bgYellow = "\033[43m";
+  static const constexpr char *bgBlue = "\033[44m";
+  static const constexpr char *bgMagenta = "\033[45m";
+  static const constexpr char *bgCyan = "\033[46m";
+  static const constexpr char *bgWhite = "\033[47m";
 
-  static constexpr char italic[] = "\033[3m";
-  static constexpr char underline[] = "\033[4m";
-  static constexpr char bold[] = "\033[1m";
+  static const constexpr char *italic = "\033[3m";
+  static const constexpr char *underline = "\033[4m";
+  static const constexpr char *bold = "\033[1m";
 };
