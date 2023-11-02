93459fa14e6e4d9433c4644fd6ae2e2a1760954c
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2023-11-02 08:29:52 +0200

    .

--- src/homework.cpp
+++ src/homework.cpp
@@ -7,19 +7,19 @@
 #define COLS 10
 #define ROWS COLS
 
-int find_max_in_matrix(std::vector<std::vector<int>> &matrix,
-                       std::function<bool(int, int)> callback);
+int find_max_in_matrix(const std::vector<std::vector<int>> &matrix,
+                       const std::function<bool(int, int)> &callback);
 
 int main() {
   std::vector<std::vector<int>> matrix(ROWS, std::vector<int>(COLS));
 
-  for (int i = 0; i < COLS; i++) {
-    for (int j = 0; j < ROWS; j++) {
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
       matrix.at(i).at(j) = COLS * i + j;
     }
   }
 
-  std::vector<std::function<bool(int, int)>> callbacks = {
+  const std::vector<const std::function<bool(int, int)>> callbacks = {
       [](int i, int j) { return i < j; },
       [](int i, int j) { return i > j; },
       [](int i, int j) { return i <= j && COLS - i > j; },
@@ -35,27 +35,27 @@ int main() {
       [](int i, int j) { return COLS - i > j + 1; },
       [](int i, int j) { return COLS - i <= j; }};
 
-  for (auto &callback : callbacks)
+  for (const auto &callback : callbacks)
     find_max_in_matrix(matrix, callback);
 }
 
-int find_max_in_matrix(std::vector<std::vector<int>> &matrix,
-                       std::function<bool(int, int)> callback) {
+int find_max_in_matrix(const std::vector<std::vector<int>> &matrix,
+                       const std::function<bool(int, int)> &callback) {
   struct Max {
     int i, j, value;
   };
   Max max = {0, 0, matrix.at(0).at(0)};
 
-  for (int i = 0; i < COLS; i++)
-    for (int j = 0; j < ROWS; j++)
+  for (int i = 0; i < ROWS; i++)
+    for (int j = 0; j < COLS; j++)
       if (callback(i, j) && matrix.at(i).at(j) > max.value)
         max = {i, j, matrix.at(i).at(j)};
 
   // printing part
-  auto print_whitespaces = [](int n) { return n < 10 ? "  " : " "; };
+  const auto print_whitespaces = [](int n) { return n < 10 ? "  " : " "; };
 
-  for (int i = 0; i < COLS; i++) {
-    for (int j = 0; j < ROWS; j++) {
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
       if (callback(i, j)) {
         if (i == max.i && j == max.j)
           std::cout << ANSI::red << ANSI::bold << matrix.at(i).at(j)
