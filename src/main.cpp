784ec487726440d388679950dedf3301d9e7b1a8
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2023-11-24 10:34:23 +0200

    selection_sort

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
 
-int main() { std::cout << typeid(unsigned long long).name() << '\n'; }
+void selection_sort(int *arr, const int size) {
+  for (int i = 0; i < size; i++) {
+    struct {
+      int index, value;
+    } max = {i, arr[i]};
+
+    for (int j = i + 1; j < size; j++) {
+      if (arr[j] > max.value)
+        max = {j, arr[j]};
+    }
+
+    std::swap(arr[i], arr[max.index]);
+  }
+}
+
+int main() {
+  int arr[] = {1, 2, 3, 4, 5};
+
+  selection_sort(arr, std::size(arr));
+
+  for (int i : arr) {
+    std::cout << i << ' ';
+  }
+  std::cout << '\n';
+}
