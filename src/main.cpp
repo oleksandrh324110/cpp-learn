a251d53c00f7a041637d60df57d3ed5cec0ab3a6
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2023-11-24 10:28:13 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,3 @@
 #include <iostream>
 
-void bubble_sort(int *arr, int size) {
-  while (size) {
-    int max_index = 0;
-    for (int i = 1; i < size; i++) {
-      if (arr[i] > arr[i - 1]) {
-        std::swap(arr[i], arr[i - 1]);
-        max_index = i;
-      }
-    }
-    size = max_index;
-  }
-}
-
-int main() {
-  int arr[] = {1, 3, 2, 5, 4};
-
-  bubble_sort(arr, std::size(arr));
-
-  for (int i : arr) {
-    std::cout << i << ' ';
-  }
-  std::cout << '\n';
-}
+int main() { std::cout << typeid(unsigned long long).name() << '\n'; }
