1e0a2b9087b375da5d7a2a109064e10a8041ee75
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2023-11-30 20:12:46 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,49 @@
 #include <iostream>
+#include <vector>
 
-template <typename T> void selection_sort(T* arr, const int size) {
-  for (int i = 0; i < size; i++) {
-    struct {
-      int index;
-      T value;
-    } max = { i, arr[i] };
+void merge(int arr[], int left, int middle, int right) {
+  int n1 = middle - left + 1;
+  int n2 = right - middle;
 
-    for (int j = i + 1; j < size; j++) {
-      if (arr[j] > max.value)
-        max = { j, arr[j] };
-    }
+  int leftArray[n1];
+  int rightArray[n2];
 
-    std::swap(arr[i], arr[max.index]);
+  for (int i = 0; i < n1; i++)
+    leftArray[i] = arr[left + i];
+  for (int j = 0; j < n2; j++)
+    rightArray[j] = arr[middle + 1 + j];
+
+  int i = 0, j = 0, k = left;
+
+  while (i < n1 && j < n2) {
+    if (leftArray[i] <= rightArray[j])
+      arr[k] = leftArray[i++];
+    else
+      arr[k] = rightArray[j++];
+    k++;
+  }
+
+  while (i < n1) arr[k++] = leftArray[i++];
+  while (j < n2) arr[k++] = rightArray[j++];
+}
+
+void mergeSort(int arr[], int left, int right) {
+  if (left < right) {
+    int middle = left + (right - left) / 2;
+
+    mergeSort(arr, left, middle);
+    mergeSort(arr, middle + 1, right);
+
+    merge(arr, left, middle, right);
   }
 }
 
 int main() {
-  int arr[] = { 1, 2, 3, 4, 5 };
+  int arr[] = { 5, 4, 3, 2, 1 };
 
-  selection_sort(arr, std::size(arr));
+  mergeSort(arr, 0, std::size(arr) - 1);
 
   for (int i : arr) {
-    std::cout << i << ' ';
-  }
-  std::cout << '\n';
+    std::cout << i << " ";
+  } std::cout << '\n';
 }
