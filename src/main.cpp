fbfef57d4c73046ad084f729f8d1375e393eb9fb
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2023-12-01 08:08:01 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
+#include <vector>
 
-template <typename T> void selection_sort(T* arr, const int size) {
-  for (int i = 0; i < size; i++) {
-    struct {
-      int index;
-      T value;
-    } max = { i, arr[i] };
+#define size 3
 
-    for (int j = i + 1; j < size; j++) {
-      if (arr[j] > max.value)
-        max = { j, arr[j] };
-    }
+struct pos {
+  int x, y;
+};
 
-    std::swap(arr[i], arr[max.index]);
+// tl = top_left, br = bottom_right
+int find_matrix_det(
+  int m[size][size],
+  pos top_left = { 0, 0 },
+  pos bottom_right = { size, size })
+{
+  if (bottom_right.x - top_left.x == 2 && bottom_right.y - top_left.y == 2) {
+    return m[top_left.x][top_left.y] * m[top_left.x + 1][top_left.y + 1] -
+      m[top_left.x + 1][top_left.y] * m[top_left.x][top_left.y + 1];
   }
 }
 
 int main() {
-  int arr[] = { 1, 2, 3, 4, 5 };
+  int arr[size][size] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
 
   std::cout << find_matrix_det(arr) << '\n';
 }
