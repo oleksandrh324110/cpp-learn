1da12e04b4962129f63c127720ff5fb46bcfd97b
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-01-16 22:31:52 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <thread>
 
 int main() {
-  std::vector<int> v = {  };
-
-  v.push_back(1);
-  v.push_back(2);
-  v.push_back(3);
-  v.push_back(4);
-  v.push_back(5);
-
-  std::cout << v.size() << ' ' << v.capacity() << '\n';
+  std::cout << not true;
 }
