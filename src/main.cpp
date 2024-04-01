ef9673b1874729e70835c9acf91dbd7fe9de76fb
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-04-01 07:59:06 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,14 @@ public:
   string(const char* str) {
     size_t str_len = strlen(str);
     _length = str_len;
-    _string = new char[str_len];
+    _string = new char[str_len + 1];
+    _string[str_len] = '\0';
     while (*_string++ = *str++) {}
     _string -= str_len + 1;
   }
   string(const string& other) : _length(other._length) {
-    _string = new char[other._length];
+    _string = new char[other._length + 1];
+    _string[_length] = '\0';
     char* other_string = other._string;
     while (*_string++ = *other_string++) {}
     _string -= _length + 1;
@@ -27,7 +29,8 @@ public:
   }
   string(size_t size) {
     _length = size;
-    _string = new char[_length];
+    _string = new char[_length + 1];
+    _string[_length] = '\0';
   }
   ~string() {
     _length = 0;
@@ -55,9 +58,24 @@ public:
   }
 
   void insert(size_t offset, const string& other) {
+    size_t length = _length + other._length;
+    char* string = new char[length];
+    char* other_string = other._string;
+    for (size_t i = 0; i < offset; i++)
+      string[i] = _string[i];
+    for (size_t i = offset; i < offset + other._length; i++)
+      string[i] = other_string[i - offset];
+    for (size_t i = offset + other._length; i < length; i++)
+      string[i] = _string[i - (offset + other._length - 1)];
+
+    _length = length;
+    delete[] _string;
+    _string = string;
+    string = nullptr;
   }
 
   inline size_t length() const { return _length; }
+  inline size_t size() const { return _length; }
 
   void print() const {
     std::cout << _string << '\n';
@@ -69,10 +87,10 @@ private:
 };
 
 int main() {
-  string str = "hello";
-  string str1 = str;
+  string str1 = "hello";
+  string str2 = "12";
 
-  str.insert(1, str1);
+  str1.insert(0, str2);
 
-  str.print();
+  str1.print();
 }
