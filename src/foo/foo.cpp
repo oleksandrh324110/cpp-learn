1d0263984d8a866c629c2b6661b15e9be0094175
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-05-13 13:47:09 +0300

    finaly moved to CMake

---
+++ src/foo/foo.cpp
@@ -0,0 +1,5 @@
+#include "foo.h"
+
+void hello_word() {
+  std::cout << "hello world!\n";
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
+#include "foo/foo.h"
 
 int main(void) {
-  std::cout << "Hello, World!" << std::endl;
-
-  return 0;
+  hello_word();
 }
