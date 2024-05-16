0dc2c8aaef67a822159d0369c61068c393b3dea3
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-05-16 17:47:04 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <vector>
+#include <numeric>
 
 int main(void) {
-  std::cout << "hello world\n";
+  size_t size; std::cout << "Enter size: "; std::cin >> size;
 
-  return 0;
+  std::vector<int> arr(size);
+
+  std::cout << "Enter elements: ";
+  for (size_t i = 0; i < size; i++)
+    std::cin >> arr[i];
+
+  std::cout << "Multiplication is: " << std::accumulate(arr.begin(), arr.end(), 1, std::multiplies<int>{}) << '\n';
 }
