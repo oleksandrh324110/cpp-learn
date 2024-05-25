31f2842e95098ae47ccc5df708b4c9d780a31d60
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-05-25 12:46:03 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,8 @@
 #include <iostream>
 
-#include "hashmap.h"
+#include "graph.h"
 
 int main(void) {
-  std::hashmap<std::string, int> map(5);
-
-  map.insert("first insert", 1);
-  map.insert("second insert", 2);
-  map["first bracket"] = 3;
-  map["second bracket"] = 4;
-
-  map.print();
-  std::cout << '\n';
-
-  map.remove("second insert");
-  map.remove("second bracket");
-
-  map.print();
-
-  std::cout << '\n' << map.contains("first insert") << '\n';
+  graph g;
+  g.distances();
 }
