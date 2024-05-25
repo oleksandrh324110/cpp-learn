40bb0b5920008800a8cc88d762b4cc22016e1259
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-05-25 16:59:23 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <vector>
 
-#include "graph.h"
+struct Genome {
+  int s, f, a, b;
+};
 
 int main(void) {
-  graph g;
-  g.distances();
+  size_t size;
+  std::cin >> size;
+
+  std::vector<std::vector<int>> genome(size, std::vector<int>(size));
+
+  for (size_t i = 0; i < size; i++) {
+    for (size_t j = 0; j < size; j++) {
+      std::cin >> genome[i][j];
+    }
+  }
+
+  for (auto& v : genome) {
+    Genome g(v[0], v[1], v[2], v[3]);
+
+    std::vector<int> vec;
+  }
 }
